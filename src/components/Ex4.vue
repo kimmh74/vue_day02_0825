1ea92b748f29038c4ef0d01b6c850654514aad41
kimmh74 <zziipp.mw@gmail.com>
2025-08-26 08:55:47 +0900

    1
    1

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -1,11 +1,32 @@
 <template>
-    <div>
-        ex1
+    <h1>v-for 예시</h1>
+    <div id="app">
+        <ol>
+            <li v-for="x in manyFoods">{{ x }}</li>
+         </ol>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const manyFoods
+    = ref(['Burrito', 'Salad', 'Cake', 'Soup', 'Fish', 'Pizza', 'Rice'])
+
 
 </script>
 
-<style scoped></style>
+<style scoped>
+#app>div {
+    border: solid black 1px;
+    width: 80%;
+    padding: 10px;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+img {
+    width: 70px;
+    margin: 10px;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    base: mode === 'production'  ?  '/vue_day02_0825/'  :  '/',         //예: /my-react-app/  
+  }
 })
